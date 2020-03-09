9796978a005551d29f86e859416469a26d5b78d3
dvnerite <57559064+dvnerite@users.noreply.github.com>
2020-03-09 12:45:54 -0700

    second commit

---
+++ src/js/components/ExpenseEntries/expenseReducer.js
@@ -0,0 +1,16 @@
+
+
+
+const defaultState = {
+    description: '',
+    amount: '',
+    lineitems: []
+
+};
+
+export default function ExpenseReducer (state = defaultState, action) {
+
+    // the `state = default state` above is new ES6 syntax
+    // for defining a default value on a parameter
+    return state;
+}
